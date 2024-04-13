01399268229abb0e7f11da6970ff279b31c9286c
Evgeniy Tatarchuk <spik_13@mail.ru>
2024-04-13 09:22:39 +0300

    config: snippets and surround plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -45,6 +45,11 @@ return {
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
+      luasnip.filetype_extend("typescript", { "typescriptreact" })
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { vim.fn.stdpath "config" .. "/snippets" },
+      }
     end,
   },
 
